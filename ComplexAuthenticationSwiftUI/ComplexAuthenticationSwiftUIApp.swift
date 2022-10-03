b17f5f8b9a6186b9464a0c08e6ca3fd1b3908130
waqas.khadim <waqas.khadim@invozone.com>
2022-10-03 17:27:16 +0500

    Initial Commit

---
+++ ComplexAuthenticationSwiftUI/ComplexAuthenticationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexAuthenticationSwiftUIApp.swift
+//  ComplexAuthenticationSwiftUI
+//
+//  Created by Waqas Khadim on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ComplexAuthenticationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
