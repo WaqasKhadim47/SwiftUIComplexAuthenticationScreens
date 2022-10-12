c958b178291b0314d98c042569f319a9aceec22d
waqas.khadim <waqas.khadim@invozone.com>
2022-10-12 16:55:58 +0500

    initial commit

---
+++ ComplexAuthenticationSwiftUI/Shared/View/SignupScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SignupScreen.swift
+//  ComplexAuthenticationSwiftUI
+//
+//  Created by Waqas Khadim on 11/10/2022.
+//
+
+import SwiftUI
+
+struct SignupScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignupScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupScreen()
+    }
+}
